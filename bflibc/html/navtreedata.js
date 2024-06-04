40c9f749335a6a6289e00e6a3502ff8127d4dbe7
Brando Fong <bfong@owc.com>
2024-06-04 13:20:34 -0700

    saving work

--- bflibc/html/navtreedata.js
+++ bflibc/html/navtreedata.js
@@ -25,10 +25,10 @@
 var NAVTREE =
 [
   [ "BFLibC", "index.html", [
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
         [ "All", "functions.html", null ],
         [ "Variables", "functions_vars.html", null ]
       ] ]
